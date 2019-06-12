c47cb25196474e221ea6ab467b3c6c2854f03360
71e6fd52 <71e6fd52@gmail.com>
2019-06-12 10:07:51 +0800

    theme

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -4,9 +4,10 @@
 
 (require-package 'color-theme-sanityinc-solarized)
 (require-package 'color-theme-sanityinc-tomorrow)
+(require-package 'monokai-theme)
 
 ;; If you don't customize it, this is the theme you get.
-(setq-default custom-enabled-themes '(sanityinc-tomorrow-bright))
+(setq-default custom-enabled-themes '(monokai))
 
 ;; Ensure that themes will be applied even if they have not been customized
 (defun reapply-themes ()
