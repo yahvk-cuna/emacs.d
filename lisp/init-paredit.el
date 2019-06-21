ba0f94a12ab09b18a31bbfcb967e47c15c2088c5
71e6fd52 <71e6fd52@gmail.com>
2019-06-21 15:49:55 +0800

    paredit space

--- lisp/init-paredit.el
+++ lisp/init-paredit.el
@@ -18,6 +18,28 @@
   (dolist (binding '("C-<left>" "C-<right>" "C-M-<left>" "C-M-<right>" "M-s" "M-?"))
     (define-key paredit-mode-map (read-kbd-macro binding) nil)))
 
+(defun paredit/space-for-delimiter-p (endp delm)
+  (or (member 'font-lock-keyword-face (text-properties-at (1- (point))))
+      (not (derived-mode-p 'basic-mode
+                           'c++-mode
+                           'c-mode
+                           'coffee-mode
+                           'csharp-mode
+                           'd-mode
+                           'dart-mode
+                           'go-mode
+                           'java-mode
+                           'js-mode
+                           'lua-mode
+                           'objc-mode
+                           'pascal-mode
+                           'python-mode
+                           'r-mode
+                           'ruby-mode
+                           'rust-mode
+                           'typescript-mode))))
+(after-load 'paredit
+  (add-to-list 'paredit-space-for-delimiter-predicates 'paredit/space-for-delimiter-p))
 
 ;; Compatibility with other modes
 
