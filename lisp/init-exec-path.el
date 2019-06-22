13442e87e0552282445fc31352b75c11a459d17d
71e6fd52 <71e6fd52@gmail.com>
2019-06-22 09:13:55 +0800

    fix path

--- lisp/init-exec-path.el
+++ lisp/init-exec-path.el
@@ -8,6 +8,7 @@
   (dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO" "LANG" "LC_CTYPE"))
     (add-to-list 'exec-path-from-shell-variables var)))
 
+(setq exec-path-from-shell-arguments nil)
 
 (when (or (memq window-system '(mac ns x))
           (unless (memq system-type '(ms-dos windows-nt))
