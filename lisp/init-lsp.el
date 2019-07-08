dc7d70e7511c25ba401c0dd6dcb3691b02efe7ff
71e6fd52 <71e6fd52@gmail.com>
2019-07-08 14:03:09 +0800

    lsp find key

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -1,11 +1,14 @@
 ;;; init-lsp --- Language Server Protocol
 ;;; Commentary:
 ;;; Code:
-(setq lsp-enable-snippet nil)
 (require-package 'lsp-mode)
 (require-package 'lsp-ui)
 (require-package 'company-lsp)
-;(require-package 'lsp-treemacs)
+(setq lsp-enable-snippet nil)
+
+(after-load 'lsp-mode
+  (define-key lsp-mode-map (kbd "M-.") 'lsp-find-definition)
+  (define-key lsp-mode-map (kbd "M-/") 'lsp-find-references))
 
 (provide 'init-lsp)
 ;;; init-lsp.el ends here
