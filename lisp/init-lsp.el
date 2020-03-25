82956b6ce5d50dfcc69c7aff38754787f9b11075
71e6fd52 <71e6fd52@gmail.com>
2020-03-25 10:43:45 +0800

    yasnippet

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -4,12 +4,13 @@
 (require-package 'lsp-mode)
 (require-package 'lsp-ui)
 (require-package 'company-lsp)
-(setq lsp-enable-snippet nil)
 (setq lsp-keymap-prefix "C-l")
 
 (after-load 'lsp-mode
   (define-key lsp-mode-map (kbd "M-.") 'lsp-find-definition)
   (define-key lsp-mode-map (kbd "M-/") 'lsp-find-references))
+
+(require-package 'yasnippet)
 
 (provide 'init-lsp)
 ;;; init-lsp.el ends here
