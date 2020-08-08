78a30faaa5ade5c3e3f1edbd82d8b3a77459223b
71e6fd52 <71e6fd52@gmail.com>
2020-08-08 12:45:29 +0800

    fix lsp completion

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -3,7 +3,6 @@
 ;;; Code:
 (require-package 'lsp-mode)
 (require-package 'lsp-ui)
-(require-package 'company-lsp)
 (setq lsp-keymap-prefix "C-l")
 
 (after-load 'lsp-mode
@@ -11,6 +10,7 @@
   (define-key lsp-mode-map (kbd "M-/") 'lsp-find-references))
 
 (require-package 'yasnippet)
+(yas-global-mode 1)
 
 (provide 'init-lsp)
 ;;; init-lsp.el ends here
