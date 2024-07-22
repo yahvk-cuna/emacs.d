781b24a113e0c464d6c77079975650f327c15f1e
71e6fd52 <71e6fd52@gmail.com>
2024-07-22 23:13:46 +0800

    customize corfu

--- lisp/init-corfu.el
+++ lisp/init-corfu.el
@@ -14,26 +14,24 @@
       completion-category-overrides nil)
 (setq completion-cycle-threshold 4)
 
-(when (maybe-require-package 'corfu)
-  (setq-default corfu-auto t)
+(use-package corfu
+  :ensure t
+  :config
   (with-eval-after-load 'eshell
     (add-hook 'eshell-mode-hook (lambda () (setq-local corfu-auto nil))))
-  (setq-default corfu-quit-no-match 'separator)
-  (add-hook 'after-init-hook 'global-corfu-mode)
-
-
-
-  (with-eval-after-load 'corfu
-    (corfu-popupinfo-mode))
-
-  ;; Make Corfu also work in terminals, without disturbing usual behaviour in GUI
-  (when (maybe-require-package 'corfu-terminal)
-    (with-eval-after-load 'corfu
-      (corfu-terminal-mode)))
-
-  ;; TODO: https://github.com/jdtsmith/kind-icon
+  (setq corfu-auto t
+        corfu-quit-no-match 'separator
+        corfu-auto-prefix 2)
+  (corfu-popupinfo-mode)
+  :init
+  (global-corfu-mode)
   )
 
+(use-package nerd-icons-corfu
+  :ensure t
+  :after corfu
+  :config
+  (add-to-list 'corfu-margin-formatters #'nerd-icons-corfu-formatter))
 
 (provide 'init-corfu)
 ;;; init-corfu.el ends here
