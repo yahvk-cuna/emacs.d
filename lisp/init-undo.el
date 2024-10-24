34503b21ca8a66a0138ce8048817242d7c899429
71e6fd52 <71e6fd52@gmail.com>
2024-10-24 11:47:57 +0800

    use vundo & undo-fo

--- lisp/init-undo.el
+++ lisp/init-undo.el
@@ -1,8 +1,12 @@
 ;;; init-undo -- undo tree
 ;;; Commentary:
 ;;; Code:
-(require-package 'undo-tree)
-(global-undo-tree-mode)
+(use-package vundo
+  :bind ("C-x u" . vundo))
+
+(use-package undo-fu
+  :bind (("C-/" . undo-fu-only-undo)
+         ("C-?" . undo-fu-only-redo)))
 
 (provide 'init-undo)
 ;;; init-undo.el ends here
