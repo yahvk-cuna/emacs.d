38c2a3eb128263231118b56b369636820917d20a
71e6fd52 <71e6fd52@gmail.com>
2025-01-03 17:04:46 +0800

    use use-package to manage copilot

--- lisp/init-copilot.el
+++ lisp/init-copilot.el
@@ -2,15 +2,16 @@
 ;;; Commentary:
 ;;; Code:
 (require-package 'editorconfig)
-(require 'copilot)
-
-(define-key copilot-completion-map (kbd "C-<tab>") 'copilot-accept-completion)
-(define-key copilot-mode-map (kbd "M-C-<next>") #'copilot-next-completion)
-(define-key copilot-mode-map (kbd "M-C-<prior>") #'copilot-previous-completion)
-(define-key copilot-mode-map (kbd "M-C-<right>") #'copilot-accept-completion-by-word)
-(define-key copilot-mode-map (kbd "M-C-<down>") #'copilot-accept-completion-by-line)
-
-(add-hook 'prog-mode-hook 'copilot-mode)
+(use-package copilot
+  :vc (:fetcher github :repo copilot-emacs/copilot.el)
+  :bind ( :map copilot-completion-map
+          ("C-<tab>" . copilot-accept-completion)
+          :map copilot-mode-map
+          ("M-C-<next>" . copilot-next-completion)
+          ("M-C-<prior>" . copilot-previous-completion)
+          ("M-C-<right>" . copilot-accept-completion-by-word)
+          ("M-C-<down>" . copilot-accept-completion-by-line))
+  :hook (prog-mode . copilot-mode))
 
 (provide 'init-copilot)
 ;;; init-copilot.el ends here
