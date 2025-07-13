a3989720b88c2d71a0ab8add4c899ac59d7fc8dc
yahvk <71e6fd52@gmail.com>
2025-07-13 22:02:03 +0800

    lua lsp

--- lisp/init-lua.el
+++ lisp/init-lua.el
@@ -2,16 +2,18 @@
 ;;; Commentary:
 ;;; Code:
 
-(require-package 'lua-mode)
-
-(setq-default lua-indent-level 2)
+(use-package lua-mode
+  :mode "\\.lua\\'"
+  :interpreter "lua"
+  :config
+  (setq lua-indent-level 2)
+  (add-hook 'lua-mode-hook #'lsp))
 
 (reformatter-define lua-format
   :program "lua-format"
   :args '("--indent-width=2" "--no-use-tab")
   :lighter "LuaFmt ")
 
-(add-hook 'lua-mode-hook #'lsp)
 
 (provide 'init-lua)
 ;;; init-lua.el ends here
