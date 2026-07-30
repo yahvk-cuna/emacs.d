45453eac0e3a60c60c44c9a72718200793651a22
yahvk <71e6fd52@gmail.com>
2026-07-30 17:53:32 +0800

    add pyright

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -22,5 +22,12 @@
 (when (maybe-require-package 'reformatter)
   (reformatter-define black :program "black" :args '("-")))
 
+(use-package lsp-pyright
+  :ensure t
+  :custom (lsp-pyright-langserver-command "pyright") ;; or basedpyright
+  :hook (python-mode . (lambda ()
+                         (require 'lsp-pyright)
+                         (lsp))))  ; or lsp-deferred
+
 (provide 'init-python)
 ;;; init-python.el ends here
