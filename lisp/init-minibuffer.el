c9b6b0b708102aa79f827b71c18ff72f4a199178
yahvk <71e6fd52@gmail.com>
2026-07-30 17:56:37 +0800

    update init-minibuffer

--- lisp/init-minibuffer.el
+++ lisp/init-minibuffer.el
@@ -9,38 +9,46 @@
   (when (maybe-require-package 'embark)
     (with-eval-after-load 'vertico
       (define-key vertico-map (kbd "C-c C-o") 'embark-export)
-      (define-key vertico-map (kbd "C-c C-c") 'embark-act)))
+      (define-key vertico-map (kbd "C-c C-c") 'embark-act)
+      (define-key vertico-map (kbd "C-c C-l") 'embark-collect)))
 
-  (when (maybe-require-package 'consult)
+  ;; https://github.com/purcell/whole-line-or-region/issues/30#issuecomment-3388095018
+  (with-eval-after-load 'embark
+    (push 'embark--mark-target
+          (alist-get 'whole-line-or-region-delete-region
+                     embark-around-action-hooks)))
+
+  (when (maybe-require-package 'embark-consult)
+    (require 'embark-consult)
     (defmacro sanityinc/no-consult-preview (&rest cmds)
       `(with-eval-after-load 'consult
          (consult-customize ,@cmds :preview-key "M-P")))
+
+    (setq xref-show-xrefs-function 'consult-xref)
 
     (sanityinc/no-consult-preview
      consult-ripgrep
      consult-git-grep consult-grep
      consult-bookmark consult-recent-file consult-xref
-     consult--source-recent-file consult--source-project-recent-file consult--source-bookmark)
+     consult-source-recent-file consult-source-project-recent-file consult-source-bookmark)
 
-    (when (and (executable-find "rg"))
-      (defun sanityinc/consult-ripgrep-at-point (&optional dir initial)
-        (interactive (list prefix-arg (when-let ((s (symbol-at-point)))
-                                        (symbol-name s))))
-        (consult-ripgrep dir initial))
-      (sanityinc/no-consult-preview sanityinc/consult-ripgrep-at-point)
+    (defun sanityinc/consult-ripgrep-at-point (&optional dir initial)
+      (interactive (list current-prefix-arg
+                         (if (use-region-p)
+                             (buffer-substring-no-properties
+                              (region-beginning) (region-end))
+                           (if-let* ((s (symbol-at-point)))
+                               (symbol-name s)))))
+      (consult-ripgrep dir initial))
+    (sanityinc/no-consult-preview sanityinc/consult-ripgrep-at-point)
+    (when (executable-find "rg")
       (global-set-key (kbd "M-?") 'sanityinc/consult-ripgrep-at-point))
 
     (global-set-key [remap switch-to-buffer] 'consult-buffer)
     (global-set-key [remap switch-to-buffer-other-window] 'consult-buffer-other-window)
     (global-set-key [remap switch-to-buffer-other-frame] 'consult-buffer-other-frame)
     (global-set-key [remap goto-line] 'consult-goto-line)
-
-
-
-    (when (maybe-require-package 'embark-consult)
-      (with-eval-after-load 'embark
-        (require 'embark-consult)
-        (add-hook 'embark-collect-mode-hook 'embark-consult-preview-minor-mode)))))
+    ))
 
 (when (maybe-require-package 'marginalia)
   (add-hook 'after-init-hook 'marginalia-mode))
