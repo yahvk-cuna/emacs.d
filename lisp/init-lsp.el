358b4d02b1edc9441559956d4c7e0a767612dc27
yahvk <71e6fd52@gmail.com>
2026-08-22 18:12:08 +0800

    update rust & lsp

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -4,56 +4,74 @@
 
 (setq lsp-use-plists t)
 
-(defun lsp-booster--advice-json-parse (old-fn &rest args)
-  "Try to parse bytecode instead of json."
-  (or
-   (when (equal (following-char) ?#)
-     (let ((bytecode (read (current-buffer))))
-       (when (byte-code-function-p bytecode)
-         (funcall bytecode))))
-   (apply old-fn args)))
-(advice-add (if (progn (require 'json)
-                       (fboundp 'json-parse-buffer))
-                'json-parse-buffer
-              'json-read)
-            :around
-            #'lsp-booster--advice-json-parse)
+;; (defun lsp-booster--advice-json-parse (old-fn &rest args)
+;;   "Try to parse bytecode instead of json."
+;;   (or
+;;    (when (equal (following-char) ?#)
+;;      (let ((bytecode (read (current-buffer))))
+;;        (when (byte-code-function-p bytecode)
+;;          (funcall bytecode))))
+;;    (apply old-fn args)))
+;; (advice-add (if (progn (require 'json)
+;;                        (fboundp 'json-parse-buffer))
+;;                 'json-parse-buffer
+;;               'json-read)
+;;             :around
+;;             #'lsp-booster--advice-json-parse)
 
-(defun lsp-booster--advice-final-command (old-fn cmd &optional test?)
-  "Prepend emacs-lsp-booster command to lsp CMD."
-  (let ((orig-result (funcall old-fn cmd test?)))
-    (if (and (not test?)                             ;; for check lsp-server-present?
-             (not (file-remote-p default-directory)) ;; see lsp-resolve-final-command, it would add extra shell wrapper
-             lsp-use-plists
-             (not (functionp 'json-rpc-connection))  ;; native json-rpc
-             (executable-find "emacs-lsp-booster"))
-        (progn
-          (message "Using emacs-lsp-booster for %s!" orig-result)
-          (cons "emacs-lsp-booster" orig-result))
-      orig-result)))
-(advice-add 'lsp-resolve-final-command :around #'lsp-booster--advice-final-command)
+;; (defun lsp-booster--advice-final-command (old-fn cmd &optional test?)
+;;   "Prepend emacs-lsp-booster command to lsp CMD."
+;;   (let ((orig-result (funcall old-fn cmd test?)))
+;;     (if (and (not test?)                             ;; for check lsp-server-present?
+;;              (not (file-remote-p default-directory)) ;; see lsp-resolve-final-command, it would add extra shell wrapper
+;;              lsp-use-plists
+;;              (not (functionp 'json-rpc-connection))  ;; native json-rpc
+;;              (executable-find "emacs-lsp-booster"))
+;;         (progn
+;;           (message "Using emacs-lsp-booster for %s!" orig-result)
+;;           (cons "emacs-lsp-booster" orig-result))
+;;       orig-result)))
+;; (advice-add 'lsp-resolve-final-command :around #'lsp-booster--advice-final-command)
 
 (use-package lsp-mode
-  :init
+  :custom
   ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
-  (setq lsp-keymap-prefix "C-l")
-  (setq lsp-eldoc-enable-hover nil)
+  (lsp-keymap-prefix "C-l")
+  (lsp-eldoc-enable-hover nil)
+  ;; (setq lsp-copilot-enabled t)
+  (lsp-copilot-enabled nil)
+  ;; (setq lsp-copilot-applicable-fn (lambda (buf-name buf-mode) (provided-mode-derived-p buf-mode 'prog-mode)))
+  (lsp-lens-enable nil)
+  (lsp-format-on-save nil)
+  ;; (lsp-format-buffer-on-save-list '(rust-mode))
+  :bind (:map lsp-mode-map ("C-l c" . lsp-execute-code-action))
+  :config
+  ;; https://github.com/emacs-lsp/lsp-mode/issues/5043
+  (remove-hook 'lsp-after-apply-edits-hook #'lsp-diagnostics--clear-after-edit)
+
   :hook (
-         ;; (XXX-mode . lsp)
          (lsp-mode . lsp-enable-which-key-integration))
   :commands lsp)
 
 (use-package lsp-ui
   :after (lsp-mode)
-  :init
-  (setq lsp-ui-sideline-show-code-actions t)
-  (setq lsp-ui-sideline-diagnostic-max-lines 10)
+  :custom
+  (lsp-ui-sideline-show-code-actions t)
+  (lsp-ui-sideline-diagnostic-max-lines 10)
+  (lsp-ui-doc-show-with-cursor t)
+  (lsp-ui-doc-position 'top)
   :commands lsp-ui-mode)
 (use-package lsp-treemacs :commands (lsp-treemacs-errors-list lsp-treemacs-symbols))
 (use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
 ;; optionally if you want to use debugger
 ;; (use-package dap-mode)
 ;; (use-package dap-LANGUAGE) to load the dap adapter for your language
+(use-package dap-mode
+  :init
+  (setq dap-gdb-debug-program '("rust-gdb" "-i" "dap"))
+  :config
+  (require 'dap-gdb))
+
 
 (require-package 'yasnippet)
 (add-hook 'lsp-mode-hook #'yas-minor-mode)
--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -1,41 +1,46 @@
 ;;; init-rust.el --- Support for the Rust language -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;; Code:
+(use-package rust-mode
+  :ensure t
+  :init
+  (setq rust-mode-treesitter-derive t))
 
 (use-package rustic
-             :ensure
-             :bind (:map rustic-mode-map
-                         ("M-j" . lsp-ui-imenu)
-                         ("M-?" . lsp-find-references)
-                         ("C-c C-c l" . flycheck-list-errors)
-                         ("C-c C-c a" . lsp-execute-code-action)
-                         ("C-c C-c r" . lsp-rename)
-                         ("C-c C-c q" . lsp-workspace-restart)
-                         ("C-c C-c Q" . lsp-workspace-shutdown)
-                         ("C-c C-c s" . lsp-rust-analyzer-status))
-             :config
-             ;; uncomment for less flashiness
-             ;; (setq lsp-eldoc-hook nil)
-             ;; (setq lsp-enable-symbol-highlighting nil)
-             ;; (setq lsp-signature-auto-activate nil)
+  :ensure
+  :after (rust-mode)
+  :demand t
+  :mode ("\\.rs\\'" . rustic-mode)
+  :hook (rustic-mode . lsp-inlay-hints-mode)
+  :bind (:map rustic-mode-map
+              ("M-j" . lsp-ui-imenu)
+              ("M-?" . lsp-find-references)
+              ("C-c C-c l" . flycheck-list-errors)
+              ("C-c C-c a" . lsp-execute-code-action)
+              ("C-c C-c r" . lsp-rename)
+              ("C-c C-c q" . lsp-workspace-restart)
+              ("C-c C-c Q" . lsp-workspace-shutdown)
+              ("C-c C-c s" . lsp-rust-analyzer-status))
+  :config
+  ;; uncomment for less flashiness
+  ;; (setq lsp-eldoc-hook nil)
+  ;; (setq lsp-enable-symbol-highlighting nil)
+  ;; (setq lsp-signature-auto-activate nil)
 
-             ;; comment to disable rustfmt on save
-             (setq rustic-format-on-save t)
-             ;; (add-hook 'rustic-mode-hook 'rk/rustic-mode-hook)
-             )
-
-(defun rk/rustic-mode-hook ()
-  ;; so that run C-c C-c C-r works without having to confirm, but don't
-  ;; try to save rust buffers that are not file visiting. Once
-  ;; https://github.com/brotzeit/rustic/issues/253 has been resolved this should
-  ;; no longer be necessary.
-  (when buffer-file-name
-    (setq-local buffer-save-without-query t))
-  (add-hook 'before-save-hook 'lsp-format-buffer nil t))
+  ;; comment to disable rustfmt on save
+  (setq rustic-format-on-save t)
+  (setq rustic-lsp-check-command "clippy")
+  )
 
 
+(setq lsp-inlay-hint-enable t)
+(setq lsp-rust-analyzer-display-chaining-hints t)
+(setq lsp-rust-analyzer-closure-capture-hints t)
+(setq lsp-rust-analyzer-display-closure-return-type-hints t)
 (setq lsp-rust-server 'rust-analyzer)
 (setq lsp-rust-analyzer-cargo-watch-command "clippy")
+(setq lsp-rust-analyzer-completion-add-call-parenthesis nil)
+
 
 
 (use-package ron-mode
